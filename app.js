6db126f952748fcf68e7a9f351fb47fe2ad39213
MIClavelTe <miclavelte@gmail.com>
2018-10-07 11:04:45 -0700

    Add questions route

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 'use strict'
 var express = require('express');
 var app = express();
+var routes = require('./routes')
 
 var jsonParser = require('body-parser').json;
 app.use(jsonParser());
 
-var port = process.env.PORT ||  7000
+app.use('/questions', routes);
+
+var port = process.env.PORT ||  3000
 app.listen(port, function() {
     console.log(`Running On Port:${port}`);
 });

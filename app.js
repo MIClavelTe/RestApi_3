ebc1e8475843469c571e6b551c70e8637b5e5928
MIClavelTe <miclavelte@gmail.com>
2018-10-07 10:58:40 -0700

    Download body parser

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict'
 var express = require('express');
-var jsonParser = require('body-parser').json;
 var app = express();
 
+var jsonParser = require('body-parser').json;
 app.use(jsonParser());
 
 var port = process.env.PORT ||  7000

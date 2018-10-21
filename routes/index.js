8e0cd7be8ecf80f8ee1f4e3101a9a55373859187
MIClavelTe <miclavelte@gmail.com>
2018-10-21 11:02:38 -0700

    Add answer routes

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,64 @@
 var express = require('express');
 var router = express.Router();
 
+//GET /questions
 router.get('/', function(req, res) {
-    res.json({response: 'Sent a GET request'});
+    res.json({response: 'Sent a GET request to /questions'});
 });
 
+//POST /questions
 router.post('/', function(req, res) {
     res.json({
-        response: 'Sent a POST request',
+        response: 'Sent a POST request to /questions',
         body: req.body
     });
 });
 
-router.get('/:id', function(req, res) {
+//GET /questions/:qID
+router.get('/:qID', function(req, res) {
     res.json({
-        response: 'Sent a GET request for ID ' + req.params.id
-    })
-})
+        response: 'Sent a GET request for /questions/' + req.params.qID
+    });
+});
+
+//POST /questions/:qID/answers
+router.post('/:qID/answers', function(req, res) {
+    res.json({
+        response: 'Sent a POST request for /questions/answers',
+        questionId: req.params.qID,
+        body: req.body
+    });
+});
+
+//PUT /questions/:qID/answers/:aID
+router.put('/:qID/answers/:aID', function(req, res) {
+    res.json({
+        response: 'Sent a PUT request for /questions/answers',
+        questionId: req.params.qID,
+        answerId: req.params.aID,
+        body: req.body
+    });
+});
+
+//DELETE /questions/:qID/answers/:aID
+router.delete('/:qID/answers/:aID', function(req, res) {
+    res.json({
+        response: 'Sent a DELETE request for /questions/answers',
+        questionId: req.params.qID,
+        answerId: req.params.aID
+    });
+});
+
+//POST /questions/:qID/answers/:aID/vote-up
+//POST /questions/:qId/answers/:aID/vote-down
+router.post('/:qID/answers/:aID/vote-:dir', function(req, res) {
+    res.json({
+        response: 'Sent a POST request for /questions/answers/vote-' + req.params.dir,
+        questionId: req.params.qID,
+        answerId: req.params.aID,
+        vote: req.params.dir
+    });
+});
 
 
 module.exports = router;

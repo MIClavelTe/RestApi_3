dbba600e8c3aefbe710d6fcc53884245258bec7e
MIClavelTe <miclavelte@gmail.com>
2019-02-14 20:00:23 -0800

    Edit app.js

--- app.js
+++ app.js
@@ -9,6 +9,20 @@ var logger = require('morgan');
 app.use(logger("dev"));
 app.use(jsonParser());
 
+'use strict'
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/qa');
+var db = mongoose.connection;
+
+db.on('error', function (err) {
+    console.error('Connection Error: ', err);
+});
+
+db.once('open', function() {
+    console.log('Connection Open');
+});
+
 app.use('/questions', routes);
 
 app.use(function(res, req, next) {

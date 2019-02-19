53d5920822be67591958d914aeee80a53d93f97a
MIClavelTe <miclavelte@gmail.com>
2019-02-18 18:20:38 -0800

    Add sortAnswers

--- model.js
+++ model.js
@@ -2,6 +2,15 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var sortAnswers = function (a, b) {
+    if (a.votes == b.votes) {
+        return b.updatedAt - a.updatedAt
+        // If a is more return -1, if b is more return 1
+    }
+    return b.votes - a.votes
+    // If a is more return -1, if b is more return 1
+};
+
 var AnswersSchema = new Schema({
     text: String,
     createdAt: {type: Date, default: Date.now},
@@ -16,7 +25,7 @@ var QuestionSchema = new Schema({
 });
 
 QuestionSchema.pre('save', function(next) {
-    this.answers.sort();
+    this.answers.sort(sortAnswers);
     next();
 });
 
